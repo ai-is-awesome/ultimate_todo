1672698d5e3eebbc21d0562674ed8d142585d319
Piyush <irock0093@gmail.com>
2020-03-02 00:31:05 +0530

    Finally added the custom user model using fieldsets and addfieldsets

--- account/admin.py
+++ account/admin.py
@@ -1,16 +1,35 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from account.models import Account
+from .forms import UserAdminCreationForm, UserAdminChangeForm
 
 
 class AccountAdmin(UserAdmin):
+	form = UserAdminChangeForm
+	add_form = UserAdminCreationForm
+
+
+
+	fieldsets = ((None, {'fields': ('email', 'username' 'password')}),
+	('Personal info', {'fields': ()}),
+	('Permissions', {'fields': ()}),
+    )
+
 	list_display = ('email','username','date_joined', 'last_login', 'is_admin','is_staff')
 	search_fields = ('email','username',)
 	readonly_fields=('date_joined', 'last_login')
 
 	filter_horizontal = ()
 	list_filter = ()
-	fieldsets = ()
+
+	add_fieldsets = (
+	(None, {
+	'classes': ('wide',),
+	'fields': ('email', 'password1', 'password2', 'username', )}
+        ), )
+
+
+
 
 
 admin.site.register(Account, AccountAdmin)
---
+++ account/forms.py
@@ -0,0 +1,95 @@
+from django import forms
+
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from .models import Account
+
+
+class RegisterForm(forms.ModelForm):
+
+	password = forms.CharField(widget = forms.PasswordInput)
+
+	password2 = forms.CharField(label = 'Confirm Password', widget = forms.PasswordInput)
+
+	class Meta:
+		model = Account
+		fields = ('email', 'username')
+
+
+	def clean_email(self):
+
+		email = self.cleaned_data.get('email')
+		qs = User.objects.filter(email = email)
+		if qs.exists():
+			raise forms.ValidationError("email is taken")
+
+		else:
+			return email
+
+	
+	def clean_password2(self):
+
+		password1 = self.cleaned_data.get("password1")
+		password2 = self.cleaned_data.get("password2")
+		if password1 and password2 and password1 != password2:
+			raise forms.ValidationError("passwords don\'t match. ")
+
+		return password2
+
+
+class UserAdminCreationForm(forms.ModelForm):
+	password1 = forms.CharField(label = "Password", widget = forms.PasswordInput)
+
+	password2 = forms.CharField(label = "Password Confirm pls", widget = forms.PasswordInput)
+
+	class Meta:
+		model = Account
+		fields = ('email', )
+
+
+	def clean_password2(self):
+		password1 = self.cleaned_data.get("password1")
+		password2 = self.cleaned_data.get("password2")
+
+		if password1 and password2 and password1 != password2:
+			raise forms.ValidationError("Passwords don't match")
+
+		else:
+			return password2
+
+
+	def save(self, commit = True):
+		user = super(UserAdminCreationForm, self).save(commit = False)
+		if commit:
+			user.save()
+		return user
+
+
+
+class UserAdminChangeForm(forms.ModelForm):
+
+	password = ReadOnlyPasswordHashField()
+
+	class Meta:
+		model = Account
+		fields = ('email', 'password', 'is_admin', 'is_active')
+
+
+	def clean_password(self):
+
+		return self.initial("password")
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
