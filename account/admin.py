2eca4f61ba46b1c3b7d65be2f2ecae457e085171
Piyush <irock0093@gmail.com>
2020-03-01 14:37:50 +0530

    Added custom admin, but won't ask the username_field

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from .models import Account
+from django.contrib.auth.admin import UserAdmin
+
 # Register your models here.
 
-admin.site.register(Account)
+class AccountAdmin(UserAdmin):
+	list_display = ('email', 'username', 'date_joined', 'last_login', 'is_admin', 
+					'is_staff'
+
+						)
+
+	search_fields = ('email', 'username', )
+
+	readonly_fields = ('last_login', 'date_joined')
+
+	filter_horizontal = ()
+
+	list_filter = ()
+
+	fieldsets = ()
+
+
+
+
+
+admin.site.register(Account, AccountAdmin)
+
