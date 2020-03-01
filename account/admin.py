419ca967cfb17b5c0bd3a7d2e8a1a620356f6c1f
Piyush <irock0093@gmail.com>
2020-03-01 23:05:53 +0530

    Added the user model

--- account/admin.py
+++ account/admin.py
@@ -1,28 +1,16 @@
 from django.contrib import admin
-from .models import Account
 from django.contrib.auth.admin import UserAdmin
+from account.models import Account
 
-# Register your models here.
 
 class AccountAdmin(UserAdmin):
-	list_display = ('email', 'username', 'date_joined', 'last_login', 'is_admin', 
-					'is_staff'
-
-						)
-
-	search_fields = ('email', 'username', )
-
-	readonly_fields = ('last_login', 'date_joined')
+	list_display = ('email','username','date_joined', 'last_login', 'is_admin','is_staff')
+	search_fields = ('email','username',)
+	readonly_fields=('date_joined', 'last_login')
 
 	filter_horizontal = ()
-
 	list_filter = ()
-
 	fieldsets = ()
 
 
-
-
-
 admin.site.register(Account, AccountAdmin)
-
