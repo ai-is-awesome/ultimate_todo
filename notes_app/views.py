1ffe83166b89346c25cb7855bb82d9b3da3b0b48
Piyush <irock0093@gmail.com>
2020-03-07 19:55:44 +0530

    Im so proud, I made the full CRUD with custom models and authentication logic, along with managing auto add current user, show stuff only to the users who are the orignal authors and not allowing anyone else to show items in the database

--- notes_app/views.py
+++ notes_app/views.py
@@ -59,20 +59,46 @@ def index(request):
 def update(request, pk):
 	context = {}
 
-	if request.user == Task.objects.get(id = pk).author:
-		if request.method == 'POST':
-			task = Task.objects.get(id = pk)
-			form = TaskForm(request.POST,instance = task)
-			if form.is_valid():
-				form.save()
-			return redirect(reverse("index"))
+
+	if request.user.is_authenticated:
+		if len(Task.objects.filter(id = pk)) != 0 and  request.user == Task.objects.filter(id = pk)[0].author:
+			if request.method == 'POST':
+				task = Task.objects.get(id = pk)
+				form = TaskForm(request.POST,instance = task)
+				if form.is_valid():
+					form.save()
+				return redirect(reverse("index"))
+
+			elif request.method != "POST":
+				try:
+					task = Task.objects.get(id = pk)
+					form = TaskForm(instance = task)
+					context["task"] = task
+					context["form"] = form
+					return render(request, 'notes_app/task_update.html', context)
 
 
-		task = Task.objects.get(id = pk)
-		form = TaskForm(instance = task)
+				except:
+					error_message  ='The following update page does\'nt exist'
 
-		context["task"] = task
-		context["form"] = form
+					return render(request, 'notes_app/index.html')
+
+
+		else:
+			return redirect(reverse('index'))
+
+
+	elif not user.is_authenticated:
+		return render(request, 'notes_app/index.html')
+
+
+
+
+
+
+
+		
+
 
 
 	return render(request, 'notes_app/task_update.html', context)
