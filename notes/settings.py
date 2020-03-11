1e182d285a7256a6d7b0a4cd3b62bbb6c7ba433a
Piyush <irock0093@gmail.com>
2020-03-11 21:27:34 +0530

    commit

--- notes/settings.py
+++ notes/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["online-todo.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
